cfcec96a9ae8d7feb1278a23b73f300059e31db1
lucianobajr <luciano.alcantara@ufv.br>
2022-10-23 18:34:49 -0300

    add

---
+++ tiny-machine-learning/ble-central/ble-central.ino
@@ -0,0 +1,83 @@
+/*
+  Arduino Nano 33 BLE Getting Started
+  BLE peripheral with a simple Hello World greeting service that can be viewed
+  on a mobile phone
+  Adapted from Arduino BatteryMonitor example
+*/
+
+#include <ArduinoBLE.h>
+
+static const char* greeting = "Hello World!";
+
+BLEService greetingService("180C");  // User defined service
+
+BLEStringCharacteristic greetingCharacteristic("2A56",BLERead, 13); // remote clients will only be able to read this
+
+
+float temp;
+int tempPin = 0;
+void setup() {
+  Serial.begin(9600);    // initialize serial communication
+  while (!Serial);
+
+  pinMode(LED_BUILTIN, OUTPUT); // initialize the built-in LED pin
+
+  if (!BLE.begin()) {   // initialize BLE
+    Serial.println("starting BLE failed!");
+    while (1);
+  }
+
+  BLE.setLocalName("Nano33BLE");  // Set name for connection
+  BLE.setAdvertisedService(greetingService); // Advertise service
+  greetingService.addCharacteristic(greetingCharacteristic); // Add characteristic to service
+  BLE.addService(greetingService); // Add service
+  greetingCharacteristic.setValue(greeting); // Set greeting string
+
+  BLE.advertise();  // Start advertising
+  Serial.print("Peripheral device MAC: ");
+  Serial.println(BLE.address());
+  Serial.println("Waiting for connections...");
+}
+
+void loop() {
+  BLEDevice central = BLE.central();  // Wait for a BLE central to connect
+
+  // if a central is connected to the peripheral:
+  if (central) {
+    Serial.print("Connected to central MAC: ");
+    // print the central's BT address:
+    Serial.println(central.address());
+    // turn on the LED to indicate the connection:
+    digitalWrite(LED_BUILTIN, HIGH);
+   
+
+    while (central.connected()){
+    // read temperature 
+    temp = analogRead(tempPin);
+    temp = temp* 0.48828125;
+    //converting analogue to temperature equivalent 
+    Serial.print("Temperature =");
+    Serial.print(temp);
+    Serial.print("C");
+    Serial.println();
+
+    greetingCharacteristic.writeValue("Temp =" + String(temp) + "C");
+
+        // Setting BLE Service Advertisment
+    BLE.setAdvertisedService(greetingService);
+    
+    // Adding the service to the BLE stack
+    BLE.addService(greetingService);
+
+    // Start advertising
+    BLE.advertise();
+    
+    delay(1000);
+    } // keep looping while connected
+    
+    
+    // when the central disconnects, turn off the LED:
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.print("Disconnected from central MAC: ");
+    Serial.println(central.address());
+  }     }
---
+++ tiny-machine-learning/ble-test/ble-test.ino
@@ -0,0 +1,150 @@
+/*
+  BLE_Central_Device.ino
+
+  This program uses the ArduinoBLE library to set-up an Arduino Nano 33 BLE Sense 
+  as a central device and looks for a specified service and characteristic in a 
+  peripheral device. If the specified service and characteristic is found in a 
+  peripheral device, the last detected value of the on-board gesture sensor of 
+  the Nano 33 BLE Sense, the APDS9960, is written in the specified characteristic. 
+
+  The circuit:
+  - Arduino Nano 33 BLE Sense. 
+
+  This example code is in the public domain.
+*/
+
+#include <ArduinoBLE.h>
+#include <Arduino_APDS9960.h>
+
+const char* deviceServiceUuid = "19b10000-e8f2-537e-4f6c-d104768a1214";
+const char* deviceServiceCharacteristicUuid = "19b10001-e8f2-537e-4f6c-d104768a1214";
+
+int gesture = -1;
+int oldGestureValue = -1;   
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  
+  if (!APDS.begin()) {
+    Serial.println("* Error initializing APDS9960 sensor!");
+  } 
+
+  APDS.setGestureSensitivity(80); 
+  
+  if (!BLE.begin()) {
+    Serial.println("* Starting Bluetooth® Low Energy module failed!");
+    while (1);
+  }
+  
+  BLE.setLocalName("Nano 33 BLE (Central)"); 
+  BLE.advertise();
+
+  Serial.println("Arduino Nano 33 BLE Sense (Central Device)");
+  Serial.println(" ");
+}
+
+void loop() {
+  connectToPeripheral();
+}
+
+void connectToPeripheral(){
+  BLEDevice peripheral;
+  
+  Serial.println("- Discovering peripheral device...");
+
+  do
+  {
+    BLE.scanForUuid(deviceServiceUuid);
+    peripheral = BLE.available();
+  } while (!peripheral);
+  
+  if (peripheral) {
+    Serial.println("* Peripheral device found!");
+    Serial.print("* Device MAC address: ");
+    Serial.println(peripheral.address());
+    Serial.print("* Device name: ");
+    Serial.println(peripheral.localName());
+    Serial.print("* Advertised service UUID: ");
+    Serial.println(peripheral.advertisedServiceUuid());
+    Serial.println(" ");
+    BLE.stopScan();
+    controlPeripheral(peripheral);
+  }
+}
+
+void controlPeripheral(BLEDevice peripheral) {
+  Serial.println("- Connecting to peripheral device...");
+
+  if (peripheral.connect()) {
+    Serial.println("* Connected to peripheral device!");
+    Serial.println(" ");
+  } else {
+    Serial.println("* Connection to peripheral device failed!");
+    Serial.println(" ");
+    return;
+  }
+
+  Serial.println("- Discovering peripheral device attributes...");
+  if (peripheral.discoverAttributes()) {
+    Serial.println("* Peripheral device attributes discovered!");
+    Serial.println(" ");
+  } else {
+    Serial.println("* Peripheral device attributes discovery failed!");
+    Serial.println(" ");
+    peripheral.disconnect();
+    return;
+  }
+
+  BLECharacteristic gestureCharacteristic = peripheral.characteristic(deviceServiceCharacteristicUuid);
+    
+  if (!gestureCharacteristic) {
+    Serial.println("* Peripheral device does not have gesture_type characteristic!");
+    peripheral.disconnect();
+    return;
+  } else if (!gestureCharacteristic.canWrite()) {
+    Serial.println("* Peripheral does not have a writable gesture_type characteristic!");
+    peripheral.disconnect();
+    return;
+  }
+  
+  while (peripheral.connected()) {
+    gesture = gestureDetectection();
+
+    if (oldGestureValue != gesture) {  
+      oldGestureValue = gesture;
+      Serial.print("* Writing value to gesture_type characteristic: ");
+      Serial.println(gesture);
+      gestureCharacteristic.writeValue((byte)gesture);
+      Serial.println("* Writing value to gesture_type characteristic done!");
+      Serial.println(" ");
+    }
+  
+  }
+  Serial.println("- Peripheral device disconnected!");
+}
+  
+int gestureDetectection() {
+  if (APDS.gestureAvailable()) {
+    gesture = APDS.readGesture();
+
+    switch (gesture) {
+      case GESTURE_UP:
+        Serial.println("- UP gesture detected");
+        break;
+      case GESTURE_DOWN:
+        Serial.println("- DOWN gesture detected");
+        break;
+      case GESTURE_LEFT:
+        Serial.println("- LEFT gesture detected");
+        break;
+      case GESTURE_RIGHT:
+        Serial.println("- RIGHT gesture detected");
+        break;
+      default:
+        Serial.println("- No gesture detected");
+        break;
+      }
+    }
+    return gesture;
+}
